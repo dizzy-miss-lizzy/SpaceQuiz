fda9581a99696bd8b498ca24fa27a7ffbfcbb6a8
Elizabeth <36939377+dizzy-miss-lizzy@users.noreply.github.com>
2018-03-13 14:07:30 -0700

    feat: Improve performance and add Submit boolean

--- app/src/main/java/com/example/android/spacequiz/MainActivity.java
+++ app/src/main/java/com/example/android/spacequiz/MainActivity.java
@@ -16,12 +16,29 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int score;
-    boolean nature, music, narration, pictures;
+    boolean isNatureChecked, isMusicChecked, isNarrationChecked, isPicturesChecked;
+    boolean isSubmitted;
+
+    EditText planetName;
+    RadioGroup radioGroupTwo, radioGroupThree, radioGroupFour, radioGroupFive, radioGroupSix;
+    CheckBox nature, music, narration, pictures;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        planetName = (EditText) findViewById(R.id.planet_view);
+        radioGroupTwo = (RadioGroup) findViewById(R.id.radio_group_two);
+        radioGroupThree = (RadioGroup) findViewById(R.id.radio_group_three);
+        radioGroupFour = (RadioGroup) findViewById(R.id.radio_group_four);
+        radioGroupFive = (RadioGroup) findViewById(R.id.radio_group_five);
+        radioGroupSix = (RadioGroup) findViewById(R.id.radio_group_six);
+        nature = (CheckBox) findViewById(R.id.nature_check_box);
+        music = (CheckBox) findViewById(R.id.music_check_box);
+        pictures = (CheckBox) findViewById(R.id.pictures_check_box);
+        narration = (CheckBox) findViewById(R.id.narration_check_box);
+        isSubmitted = false;
     }
 
     /**
@@ -162,104 +179,79 @@ public class MainActivity extends AppCompatActivity {
         // Check which checkbox was clicked
         switch (view.getId()) {
             case R.id.nature_check_box:
-                nature = checked;
+                isNatureChecked = checked;
                 break;
             case R.id.music_check_box:
-                music = checked;
+                isMusicChecked = checked;
                 break;
             case R.id.narration_check_box:
-                narration = checked;
+                isNarrationChecked = checked;
                 break;
             case R.id.pictures_check_box:
-                pictures = checked;
+                isPicturesChecked = checked;
                 break;
         }
     }
 
     /**
-     * Executes when Submit button is clicked. Scores for question #1 and #7 are determined.
+     * Executes when Submit button is clicked. Scores for questions #1 and #7 are determined.
      */
     public void submitScore(View view) {
-        // Displays error toast message if user clicks Submit button too many times
-        if (score == 7) {
-            Toast.makeText(getApplicationContext(), "Score cannot exceed 7", Toast.LENGTH_SHORT).show();
-            // Exits method early
-            return;
-        }
-
-        /**
-         * Question 1: EditText
-         * Correct answer: Venus
-         */
-        EditText planetName = (EditText) findViewById(R.id.planet_view);
-        String planetInput = planetName.getText().toString().trim();  // Gets planet name input
-
-        if (planetInput.equals("Venus")) {
-            score += 1;
+        // Check for isSubmitted value
+        if (isSubmitted) {
+            Toast.makeText(getApplicationContext(), "You've already submitted your answers", Toast.LENGTH_SHORT).show();
         } else {
-            score += 0;
+            /**
+             * Question 1: EditText
+             * Correct answer: Venus
+             */
+            if (planetName.getText().toString().trim().equals("Venus")) {
+                score += 1;
+            } else {
+                score += 0;
+            }
+
+            // Checks answers for question 7
+            // Will only return a score of 1 if ALL correct answers are selected
+            if (nature.isChecked() && music.isChecked() && pictures.isChecked() && !narration.isChecked()) {
+                score += 1;
+            } else {
+                score += 0;
+            }
+
+            // Displays toast message with user's score and changes isSubmitted value
+            Toast.makeText(getApplicationContext(), "Your score: " + score + " out of 7", Toast.LENGTH_LONG).show();
+            isSubmitted = true;
         }
-
-        // Checks answers for question #7
-        // Will only return a score of 1 if ALL correct answers are selected
-        CheckBox nature = (CheckBox) findViewById(R.id.nature_check_box);
-        CheckBox music = (CheckBox) findViewById(R.id.music_check_box);
-        CheckBox pictures = (CheckBox) findViewById(R.id.pictures_check_box);
-        CheckBox narration = (CheckBox) findViewById(R.id.narration_check_box);
-
-        if (nature.isChecked() && music.isChecked() && pictures.isChecked() && !narration.isChecked()) {
-            score += 1;
-        } else {
-            score += 0;
-        }
-
-        // Displays toast message with user's score
-        Toast.makeText(getApplicationContext(), "Your score: " + score + " out of 7", Toast.LENGTH_LONG).show();
     }
 
     public void resetScore(View view) {
-        // Resets score
+        // Resets score and boolean for Submit
         score = 0;
+        isSubmitted = false;
 
         // Resets EditText for question 1
-        EditText planetName = (EditText) findViewById(R.id.planet_view);
         planetName.getText().clear();
 
         // Resets radio buttons for questions 2, 3, 4, 5, and 6
-        RadioGroup radioGroupTwo = (RadioGroup) findViewById(R.id.radio_group_two);
         radioGroupTwo.clearCheck();
-
-        RadioGroup radioGroupThree = (RadioGroup) findViewById(R.id.radio_group_three);
         radioGroupThree.clearCheck();
-
-        RadioGroup radioGroupFour = (RadioGroup) findViewById(R.id.radio_group_four);
         radioGroupFour.clearCheck();
-
-        RadioGroup radioGroupFive = (RadioGroup) findViewById(R.id.radio_group_five);
         radioGroupFive.clearCheck();
-
-        RadioGroup radioGroupSix = (RadioGroup) findViewById(R.id.radio_group_six);
         radioGroupSix.clearCheck();
 
         // Resets check boxes for question 7
-        CheckBox nature = (CheckBox) findViewById(R.id.nature_check_box);
         if (nature.isChecked()) {
             nature.setChecked(false);
         }
-        CheckBox music = (CheckBox) findViewById(R.id.music_check_box);
         if (music.isChecked()) {
             music.setChecked(false);
         }
-        CheckBox narration = (CheckBox) findViewById(R.id.narration_check_box);
         if (narration.isChecked()) {
             narration.setChecked(false);
         }
-        CheckBox pictures = (CheckBox) findViewById(R.id.pictures_check_box);
         if (pictures.isChecked()) {
             pictures.setChecked(false);
         }
-
     }
 }
-
-
